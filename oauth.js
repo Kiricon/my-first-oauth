24a13e1b4235440c401c065ff1e1b8fa7183af2f
Dominic Valenciana <kiricon@live.com>
2018-02-01 17:55:43 -0700

    initial fetch setup

--- oauth.js
+++ oauth.js
@@ -1,1 +1,13 @@
-alert("wooooo");
+const consumerKey = 'Tps0sZFXGso33E2OUhhIIJHNj';
+const requestTokenURL = 'https://api.twitter.com/oauth/request_token';
+
+async function main() {
+    fetch(requestTokenURL)
+        .then(async (response) => [
+            console.log(await response.text())
+        ]).catch((reason) => {
+            console.log(reason);
+        });
+}
+
+main();
